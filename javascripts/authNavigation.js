5f16ca31804c0e410521c682836dccfa88399106
Pierre Merlin <pierre@stootie.com>
2014-11-10 16:38:49 +0100

    Add script to load data from private repo.

---
+++ javascripts/authNavigation.js
@@ -0,0 +1,42 @@
+// from [Kim Lane](http://kinlane.com/2013/10/15/securing-site-that-runs-on-github-pages-with-json-backend-in-private-repository/)
+
+function getUrlVar(key){
+	var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search); 
+	return result && unescape(result[1]) || ""; 
+  }
+
+// Set the page
+$ThisPage = getUrlVar('p');    
+if($ThisPage==''){
+	$ThisPage = "Home";
+}    
+
+// Set the oauth token
+$token = getUrlVar('t');
+
+var github = new Github({
+	token: $token,
+	auth: "oauth"
+	});
+
+var repo = github.getRepo('pimpin', 'gitGouv');
+
+// Pull the JSON file for site
+repo.read('master','site.json',function(err, data){            
+	if(!err) {
+		$Site = jQuery.parseJSON(data);
+
+		// Build the navigation
+		$.each($Site['navigation'], function($key, $value) {
+			$NavigationString = '<li><a href="' + $value + '">' + $key + '</a></li>';
+			$("#primaryNavItems").append($NavigationString);           
+		});
+		
+		// Build the Page Content
+		$.each($Site['pages'], function($key, $value) {
+			if($key==$ThisPage){
+				$("#pageBody").html($value);
+				}             
+		});  
+	 }          
+});
